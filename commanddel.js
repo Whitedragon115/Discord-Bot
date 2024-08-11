9722509ca41a7b86bbb57622f0c3e7ab4a69b57e
GodwhiteTaiwan <89009069+GodwhiteTaiwan@users.noreply.github.com>
2024-08-11 15:23:53 +0800

    format, Improve program syntax and add error handler

--- commanddel.js
+++ commanddel.js
@@ -1,19 +1,18 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-require('dotenv').config();
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+require("dotenv").config();
 
 const token = process.env.DISCORD_TOKEN;
-const clientId = 1161610985690374184n
-const guildId = 1161357736819302500n
-    
-const rest = new REST({ version: '9' }).setToken(token);
-rest.get(Routes.applicationGuildCommands(clientId, guildId))
-    .then(data => {
-        const promises = [];
-        for (const command of data) {
-            const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
-            promises.push(rest.delete(deleteUrl));
-        }
-        return Promise.all(promises);
-    });
+const clientId = 1161610985690374184n;
+const guildId = 1161357736819302500n;
+
+const rest = new REST({ version: "9" }).setToken(token);
+rest.get(Routes.applicationGuildCommands(clientId, guildId)).then((data) => {
+  const promises = [];
+  for (const command of data) {
+    const deleteUrl = `${Routes.applicationGuildCommands(clientId, guildId)}/${command.id}`;
+    promises.push(rest.delete(deleteUrl));
+  }
+  return Promise.all(promises);
+});
